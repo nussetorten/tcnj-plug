8b722dc959a48e2766f94e8be606b950b70e5882
Nicholas Vitovitch <ngvitovitch@gmail.com>
2013-02-27 19:37:06 -0500

    Adding basic rails project directories, files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plug::Application.config.secret_token = '40a8350ff43c12371594c2b22dcc678a8be057aeace70af2376e52f838549cefdb86b90ba252eb50b3ac76074c192959b9621052b85bf96827ac302cb987217d'
