3e96e27aa45b2510044e2bdf2e2169c8821b1120
Nicholas Vitovitch <ngvitovitch@gmail.com>
2013-03-03 19:19:57 -0500

    Added Rails 3.2.12 empty project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TcnjPlug::Application.config.secret_token = '20f2ca5dfe802de2eb3165e17265b891f36275b206158b336b4c0a9bfd652cef9bf3ad13545d8f9ed6119bb87fa1912e130cfa296879b7eb792ffb53c629edc8'
