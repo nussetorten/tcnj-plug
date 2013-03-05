54583c762529a9798f0e2b4f92d8652b912266bc
Nicholas Vitovitch <ngvitovitch@gmail.com>
2013-03-04 23:49:23 -0500

    First iteration of UI complete.  Templated tiles, tags

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,2 +1,12 @@
-$(document).ready(function() {
+$(function() {
+    $('#tags').sortable({ axis: "x" });
+
+    $('li.lbutton').click(function() {
+	$('ul#tiles > li.tile:first').appendTo('ul#tiles-hideout');
+	$('ul#tiles-hideout > li.tile:first').insertBefore('ul#tiles > li.rbutton');	
+    });
+    $('li.rbutton').click(function() {
+	$('ul#tiles > li.tile:last').prependTo('ul#tiles-hideout');
+	$('ul#tiles-hideout > li.tile:last').insertAfter('ul#tiles > li.lbutton');	
+    });
 });
